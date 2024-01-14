d761ce9b6245e50a60023708c346a9753ee87944
Baked Pineapple <bakedpineapple@airmail.cc>
2024-01-14 01:13:51 -0800

    potential onstart fix

--- onstart.sh
+++ onstart.sh
@@ -1,2 +1,1 @@
-cd /root/StyleTTS2
-accelerate launch --mixed_precision=fp16 train_second_segmented.py --config_path ./Configs/config_resume.yml &
+(cd /root/StyleTTS2; accelerate launch --mixed_precision=fp16 train_second_ddp.py --config_path ./Configs/config_resume.yml)
