efbc42645c613b0823c378080394d2fcb5acffec
Baked Pineapple <bakedpineapple@airmail.cc>
2024-01-14 09:25:11 -0800

    fix bash scripts

--- train_second.sh
+++ train_second.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 
 while true; do
-    accelerate launch --mixed_precision=fp16 train_second_ddp.py --config_path = ./Configs/config.yml
-    if [ -e oom status ]; then
+    accelerate launch --mixed_precision=fp16 train_second_ddp.py --config_path ./Configs/config.yml
+    if [ -e oom_status ]; then
         echo "OOM status detected. Re-running the script."
         rm oom_status
     else
